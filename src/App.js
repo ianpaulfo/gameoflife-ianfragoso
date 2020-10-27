85f2e528a86582f3310662146f046ea7926b4e50
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-10-26 21:45:28 -0700

    implemented grid size adjustment button and some basic styling

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import Grid from './Grid';
 class App extends React.Component {
   constructor () {
     super ();
-    this.state = {
-      generations: 0,
-    }
   }
 
   render () {
     return (
       <div className="App">
         <h1>Conway's Game of Life</h1>
-        <Grid></Grid>
+        <div className="grid">
+          <Grid></Grid>
+        </div>
+        <div>
+          <h2>Rules:</h2>
+          <p>Any live cell with two or three live neighbours survives.</p>
+          <p>Any dead cell with three live neighbours becomes a live cell.</p>
+          <p>All other live cells die in the next generation. Similarly, all other dead cells stay dead.</p>
+          <p>!Life, finds a way!</p>
+        </div>
       </div>
       )
     }
--- src/Grid.js
+++ src/Grid.js
@@ -1,10 +1,13 @@
 import React, { useCallback, useState, useRef} from 'react';
 import './App.css';
-import produce from 'immer';
+import produce, { setAutoFreeze } from 'immer';
 
 
-const numRows = 34;
-const numColumns = 34;
+
+var numRows = 25;
+var numColumns = 25;
+
+var generation = 0
 
 //represents the number of neighbor variability
 const operations = [
@@ -18,16 +21,14 @@ const operations = [
     [-1, 0]
   ];
 
-const generateEmptyGrid = () => {
+const generateEmptyGrid = () => {    
     const rows = [];
     for (let i = 0; i < numRows; i++) {
       rows.push(Array.from(Array(numColumns), () => 0));
     }
-  
+    console.log("rows:", rows)
     return rows;
-  };
-
-var generation = 0
+};
 
 
 const Grid = () => {
@@ -40,10 +41,8 @@ const Grid = () => {
     generation += 1;
   }, [])
 
-  
 
   const [running, setRunning] = useState(false);
-
 //keeps state of runningRef up to date with running for callback
   const runningRef = useRef(running);
   runningRef.current = running;
@@ -51,9 +50,7 @@ const Grid = () => {
 // Algorithm to generate new generations of cells using rules provided:
 // Recursion method is used as a callback function 
 // BigO notation -> Log(n)
-  const runSimulation = useCallback(() => {
-
-    
+  const runSimulation = useCallback(() => { 
     //base case
     updateGeneration(generation);
     if (!runningRef.current) {
@@ -88,20 +85,15 @@ const Grid = () => {
   }, []);
 
   const runSingleSimulation = useCallback(() => {
-
-    
-    //base case
     updateGeneration(generation);
     if (!runningRef.current) {
         return;
     } 
     setGrid((currentGrid) => {
-        //double-buffer function to measure every cell in that grid against Conway’s rules, create a new Grid accordingly, then replace the old Grid with the new one.
         return produce(currentGrid, gridCopy => {
             for (let i = 0; i < numRows; i++) {
                 for (let j = 0; j < numColumns; j++) {
                     let neighbors = 0;
-                    //count the number of live neighbors
                     operations.forEach(([x, y]) => {
                         const newI = i + x;
                         const newJ = j + y;
@@ -109,7 +101,6 @@ const Grid = () => {
                           neighbors += currentGrid[newI][newJ];
                         }
                     });
-                    //based on rules for game of life, cell life is determined by checking number of neighbor requirements
                     if (neighbors < 2 || neighbors > 3) {
                         gridCopy[i][j] = 0;
                     } else if (currentGrid[i][j] === 0 && neighbors === 3) {
@@ -123,20 +114,22 @@ const Grid = () => {
     console.log("Single Generation -> currently:", generation)
   }, []);
 
+
   return (
     <div>
         <button
             onClick={() => {
             console.log("start button pressed")
-            setRunning(!running);
+            setRunning(!running); 
             if (!running) {
                 runningRef.current = true;
                 runSimulation();
             }
-            }}
+        }}
         >
             {running ? "Pause" : "Start"}
         </button>
+
         <button
             onClick={() => {
                 console.log("Single generation button pressed")
@@ -146,46 +139,74 @@ const Grid = () => {
                 }
                 runningRef.current = true;
                 runSingleSimulation()
-            }
-            }
+            }}
         >
             Single Generation
         </button>
+
+        <button
+            onClick={() => {
+                if (numRows >= 55) {
+                    numRows = 15
+                    numColumns = 15
+                    return
+                }
+                console.log("grid size button pressed", numRows, numColumns)
+                generation = 0
+                numRows += 10
+                numColumns += 10
+                setGrid(generateEmptyGrid());
+                if (running) {
+                    setRunning(!running)
+                    return
+                }                
+            }}
+        >
+            Grid Size:{numColumns}x{numRows}
+        </button>
+
         <button
             onClick={() => {
             console.log("clear button pressed")
             generation = 0
             setGrid(generateEmptyGrid());
+            if (running) {
+                setRunning(!running)
+                return
+            }
             }}
         >
             Clear
         </button>
+
         <button
             onClick={() => {
+                if (running) {
+                    return
+                }
                 const rows = [];
                 for (let i = 0; i < numRows; i++) {
                 rows.push(Array.from(Array(numColumns), () => Math.random() > .85 ? 1 : 0));
     }
-  
-                setGrid(rows);
+            generation = 0  
+            setGrid(rows);
             }}
         >
             Random Seed
         </button>
 
-        {/* <button onClick={() => setSpeed(speed * 10)}>
-            Speed x10
-         </button> */}
-
-        <div style={{
+        <div className="grid" style={{
             display: 'grid',
-            gridTemplateColumns: `repeat(${numColumns}, 21px)`
+            gridTemplateColumns: `repeat(${numColumns}, 23px)`
             }}>
             {grid.map((rows, i) => 
                 rows.map((columns, j) => 
                 <div 
                     key = {`${i}-${j}`}
                     onClick={() => {
+                        if (running) {
+                            return
+                        }
                         //making a mutable change by generating a new grid
                         const newGrid = produce(grid, gridCopy => {
                             gridCopy[i][j] = grid[i][j] ? 0 : 1;
@@ -195,7 +216,7 @@ const Grid = () => {
                     style={{
                     width: 20, 
                     height: 20, 
-                    backgroundColor: grid[i][j] ? "green": undefined,
+                    backgroundColor: grid[i][j] ? "green": "yellow",
                     border: "solid 2px black"
                     }} 
                 />
